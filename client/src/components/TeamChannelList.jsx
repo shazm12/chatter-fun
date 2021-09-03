5930c2301963dec767503d751113b0b3c9eeae10
Shamik <64892076+shazm12@users.noreply.github.com>
2021-09-03 20:48:45 +0530

    Add files via upload

---
+++ client/src/components/TeamChannelList.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { AddChannel } from '../assets'
+
+
+const TeamChannelList = ({ children, error=false, loading, type, isCreating, setIsCreating, setIsEditing ,setCreateType }) => {
+    
+    if(error) {
+        return type=== 'team' ? (
+
+            <div className="team-channel-list">
+                <div className="team-channel-list__message">
+                    Connection failed, please try again
+
+                </div>
+
+            </div>
+
+        ): null
+    }
+    if(loading) {
+        return (
+            <div className="team-channel-list">
+                <p className="team-channel-list__message loading">
+                    {type==='team' ? 'Channels' : 'Messages'} loading...
+                </p>
+
+            </div>
+
+        )
+    }
+    
+    
+    return (
+            <div className="team-channel-list">
+                <div className="team-channel-list__header">
+                    <p className="team-channel-list__header__title">
+                        {type==='team' ? 'Channels': 'Direct Messages'}
+                    </p>
+                        <AddChannel
+                            isCreating={isCreating} 
+                            setIsCreating={setIsCreating}
+                            setIsEditing={setIsEditing} 
+                            setCreateType={setCreateType} 
+                            type= { type ==='team' ? 'team' : 'messaging'}
+                        />
+                        
+                </div>
+            {children}
+            
+        </div>
+    )
+}
+
+export default TeamChannelList
